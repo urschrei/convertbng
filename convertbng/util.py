b758d44a25f96759b41f87bd093e7058e24b5ac0
Stephan Hügel <urschrei@gmail.com>
2015-06-22 20:11:45 +0100

    Specify arg types

--- convertbng/util.py
+++ convertbng/util.py
@@ -10,7 +10,7 @@ else:
     ext = "so"
 
 __author__ = u"Stephan Hügel"
-__version__ = "0.1.7"
+__version__ = "0.1.8"
 
 # hacky: http://stackoverflow.com/a/30789980/416626
 class Int32_2(Structure):
@@ -20,9 +20,9 @@ class Int32_2(Structure):
 file_path = os.path.dirname(__file__)
 lib = cdll.LoadLibrary(os.path.join(file_path, 'liblonlat_bng.' + ext))
 rust_bng = lib.convert
+rust_bng.argtypes = [c_float, c_float]
 rust_bng.restype = Int32_2
 
 def convertbng(lon, lat):
     """ Simple wrapper around the linked Rust function """
-    c_lon, c_lat = c_float(lon), c_float(lat)
-    return tuple(r for r in rust_bng(c_lon, c_lat).array)
+    return tuple(r for r in rust_bng(lon, lat).array)
