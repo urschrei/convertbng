f5c958e9b4e501db5ecf7083a5a7a39c4bb93df3
Stephan Hügel <urschrei@gmail.com>
2016-01-12 12:34:22 +0000

    Tidying up Python interfaces

--- convertbng/util.py
+++ convertbng/util.py
@@ -54,19 +54,15 @@ rust_bng = lib.convert_vec_c
 rust_bng.argtypes = [c_float, c_float]
 rust_bng.restype = Int32_2
 
-def convertbng(lon, lat):
-    """ Simple wrapper around the linked Rust function """
-    return tuple(r for r in rust_bng(lon, lat).array)
-
 
 class BNG_FFITuple(Structure):
     _fields_ = [("a", c_uint32),
                 ("b", c_uint32)]
 
+
 class BNG_FFIArray(Structure):
     _fields_ = [("data", c_void_p),
                 ("len", c_size_t)]
-
     # Allow implicit conversions from a sequence of 32-bit unsigned
     # integers.
     @classmethod
@@ -94,10 +90,10 @@ class LONLAT_FFITuple(Structure):
     _fields_ = [("a", c_float),
                 ("b", c_float)]
 
+
 class LONLAT_FFIArray(Structure):
     _fields_ = [("data", c_void_p),
                 ("len", c_size_t)]
-
     # Allow implicit conversions from a sequence of 32-bit unsigned
     # integers.
     @classmethod
@@ -111,6 +107,7 @@ class LONLAT_FFIArray(Structure):
         raw_seq = array_type(*seq)
         self.data = cast(raw_seq, c_void_p)
         self.len = len(seq)
+
 
 # A conversion function that cleans up the result value to make it
 # nicer to consume.
@@ -145,8 +142,14 @@ drop_ll_array = lib.drop_float_array
 drop_ll_array.argtypes = (LONLAT_FFIArray,)
 drop_ll_array.restype = None
 
+def convertbng(lon, lat):
+    """ Single-threaded lon, lat --> BNG conversion """
+    return tuple(r for r in rust_bng(lon, lat).array)
+
 def convertbng_list(lons, lats):
-    return [(i.a, i.b) for i in iter(convert_bng(lons, lats))]
+    """ Multi-threaded lon, lat --> BNG conversion """
+    return convert_bng(lons, lats)
 
 def convertlonlat_list(eastings, northings):
-    return [(i.a, i.b) for i in iter(convert_lonlat(eastings, northings))]
+    """ Multi-threaded BNG --> lon, lat conversion """
+    return convert_lonlat(eastings, northings)
