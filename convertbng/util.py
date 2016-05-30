1d95287c69716fe08c3023554d825028639b8a76
Stephan Hügel <urschrei@gmail.com>
2016-05-30 12:58:49 +0200

    Version bump

--- convertbng/util.py
+++ convertbng/util.py
@@ -41,7 +41,7 @@ else:
     ext = "so"
 
 __author__ = u"Stephan Hügel"
-__version__ = "0.4.6"
+__version__ = "0.4.7"
 
 file_path = os.path.dirname(__file__)
 lib = cdll.LoadLibrary(os.path.join(file_path, 'liblonlat_bng.' + ext))
