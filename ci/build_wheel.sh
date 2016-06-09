fe7be374b2830b89235097113a00baddae172eb2
Stephan Hügel <urschrei@gmail.com>
2016-06-09 12:38:35 +0200

    Don't repair wheels for now

--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -10,7 +10,7 @@ yum install -y atlas-devel
 # Bundle external shared libraries into the wheels
 for whl in wheelhouse/*.whl; do
     auditwheel show $whl
-    auditwheel repair $whl -w /io/wheelhouse/
+    # auditwheel repair $whl -w /io/wheelhouse/
 done
 
 # Install packages and test
