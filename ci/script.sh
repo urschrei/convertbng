486c6eb8709b0e37aae30085347276775d937d99
Stephan Hügel <urschrei@gmail.com>
2016-06-09 14:12:05 +0200

    List convertbng contents

--- ci/script.sh
+++ ci/script.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 set -e -x
+ls ../convertbng/convertbng
 nosetests -v --with-xcoverage --cover-package=convertbng --cover-tests
 
 
 if [[ "$DOCKER_BUILD" == "TRUE" ]]; then
     docker run --rm -v `pwd`:/io $DOCKER_IMAGE $PRE_CMD /io/ci/build_wheel.sh
-    pwd
-    ls
     ls wheelhouse/
 fi
