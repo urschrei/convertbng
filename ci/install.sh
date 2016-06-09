18029d455344e41a2896fdcc220746d915597128
Stephan Hügel <urschrei@gmail.com>
2016-06-09 19:39:16 +0200

    Homebrew install Python 2 on OS X

--- ci/install.sh
+++ ci/install.sh
@@ -2,16 +2,22 @@
 set -e -x
 if [[ "$DOCKER_BUILD" == "TRUE" ]]; then
     docker pull $DOCKER_IMAGE
+    sudo pip install pip --upgrade
+    sudo pip install --install-option="--no-cython-compile" cython
+    sudo pip install numpy
+    sudo pip install python-coveralls
+    sudo pip install nosexcover
 fi
-sudo pip install pip --upgrade
-sudo pip install --install-option="--no-cython-compile" cython
-sudo pip install numpy
-sudo pip install python-coveralls
-sudo pip install nosexcover
 ls $HOME/build/urschrei/convertbng
 if [[ "$OSX_BUILD" == "TRUE" ]]; then
     echo "OSX!"
     cc --version
+    source venv/bin/activate
+    pip install pip --upgrade
+    pip install --install-option="--no-cython-compile" cython
+    pip install numpy
+    pip install python-coveralls
+    pip install nosexcover
     # otool -L $HOME/build/urschrei/convertbng/convertbng/liblonlat_bng.dylib
     # mkdir -p $HOME/build/urschrei/lonlat_bng/target/x86_64-apple-darwin/release/
     # cp $HOME/build/urschrei/convertbng/convertbng/liblonlat_bng.dylib $HOME/build/urschrei/lonlat_bng/target/x86_64-apple-darwin/release/ 
