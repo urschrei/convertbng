09ca7f03eded142cb3f8d2a084f5eb36514b129d
Stephan Hügel <urschrei@gmail.com>
2016-06-09 16:56:36 +0200

    More env var fiddling

--- ci/install.sh
+++ ci/install.sh
@@ -8,7 +8,8 @@ sudo pip install --install-option="--no-cython-compile" cython
 sudo pip install numpy
 sudo pip install python-coveralls
 sudo pip install nosexcover
-if ! [[ -v "$DOCKER_BUILD" ]]; then
+
+if [[ "$OSX_BUILD" == "TRUE" ]]; then
     system_profiler SPSoftwareDataType | grep "System Version" | awk '{print $6}'
     otool -L $HOME/build/urschrei/convertbng/convertbng/liblonlat_bng.dylib
     # mkdir -p $HOME/build/urschrei/lonlat_bng/target/x86_64-apple-darwin/release/
