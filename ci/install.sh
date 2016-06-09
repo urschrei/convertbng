3d5424fd69b90970078ae265a55bdf73aaad60ee
Stephan Hügel <urschrei@gmail.com>
2016-06-09 16:45:03 +0200

    Test for docker

--- ci/install.sh
+++ ci/install.sh
@@ -8,7 +8,7 @@ sudo pip install --install-option="--no-cython-compile" cython
 sudo pip install numpy
 sudo pip install python-coveralls
 sudo pip install nosexcover
-if [[ "$DOCKER_BUILD" != "TRUE" ]]; then
+if ! [[ -v "$DOCKER_BUILD" ]]; then
     system_profiler SPSoftwareDataType | grep "System Version" | awk '{print $6}'
     otool -L $HOME/build/urschrei/convertbng/convertbng/liblonlat_bng.dylib
     # mkdir -p $HOME/build/urschrei/lonlat_bng/target/x86_64-apple-darwin/release/
