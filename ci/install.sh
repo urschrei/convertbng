e220fda68d4ca3d81363f300acea93936f0b8093
Stephan Hügel <urschrei@gmail.com>
2016-06-09 18:12:19 +0200

    Always list dir contents

--- ci/install.sh
+++ ci/install.sh
@@ -8,9 +8,8 @@ sudo pip install --install-option="--no-cython-compile" cython
 sudo pip install numpy
 sudo pip install python-coveralls
 sudo pip install nosexcover
-
+ls $HOME/build/urschrei/convertbng
 if [[ "$OSX_BUILD" == "TRUE" ]]; then
-    ls $HOME/build/urschrei/convertbng/convertbng
     # otool -L $HOME/build/urschrei/convertbng/convertbng/liblonlat_bng.dylib
     # mkdir -p $HOME/build/urschrei/lonlat_bng/target/x86_64-apple-darwin/release/
     # cp $HOME/build/urschrei/convertbng/convertbng/liblonlat_bng.dylib $HOME/build/urschrei/lonlat_bng/target/x86_64-apple-darwin/release/ 
