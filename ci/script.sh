e2738c3a50de856a6a6f20e5e6b30192f6872367
Stephan Hügel <urschrei@gmail.com>
2016-06-09 14:28:49 +0200

    Move nose back to top-level dir

--- ci/script.sh
+++ ci/script.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 set -e -x
-ls ../convertbng/convertbng
-nosetests -v --with-xcoverage --cover-package=convertbng --cover-tests
-
 
 if [[ "$DOCKER_BUILD" == "TRUE" ]]; then
     docker run --rm -v `pwd`:/io $DOCKER_IMAGE $PRE_CMD /io/ci/build_wheel.sh
