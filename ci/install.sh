fa49d6762bc7057ff83e2f5601529ad6661976c0
Stephan Hügel <urschrei@gmail.com>
2016-06-09 12:04:56 +0200

    Try to conditionally run Docker tests

---
+++ ci/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e -x
+
+if [[ "$DOCKER_BUILD" == "TRUE" ]]; then
+    docker pull $DOCKER_IMAGE
+fi
+sudo pip install pip --upgrade
+sudo pip install --install-option="--no-cython-compile" cython
+sudo pip install numpy
+sudo pip install python-coveralls
+sudo pip install nosexcover
+sudo pip install -e .
---
+++ ci/script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e -x
+
+nosetests -v --with-xcoverage --cover-package=convertbng --cover-tests
+
+
+if [[ "$DOCKER_BUILD" == "TRUE" ]]; then
+    docker run --rm -v `pwd`:/io $DOCKER_IMAGE $PRE_CMD /io/ci/build_wheel.sh
+    ls wheelhouse/
+fi
