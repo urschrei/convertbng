e76d66673fd792ac95db78a83900a39069359dc2
Stephan Hügel <urschrei@gmail.com>
2016-06-09 17:54:36 +0200

    Remove $ORIGIN bc the Travis linker doesn't like it

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ extensions = Extension("convertbng.cutil",
                     library_dirs=['.', 'convertbng'],
                     # from http://stackoverflow.com/a/10252190/416626
                     # the $ORIGIN trick is not perfect, though
-                    runtime_library_dirs=['$ORIGIN'],
+                    runtime_library_dirs=['.'],
                     extra_compile_args=["-O3"],
 )
 
