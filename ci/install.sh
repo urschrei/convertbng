2dd2c22d0f8c2ae2d2c60bffcf61560f236ece9c
Stephan Hügel <urschrei@gmail.com>
2016-06-09 14:54:19 +0200

    More path wrangling

--- ci/install.sh
+++ ci/install.sh
@@ -9,4 +9,4 @@ sudo pip install --install-option="--no-cython-compile" cython
 sudo pip install numpy
 sudo pip install python-coveralls
 sudo pip install nosexcover
-sudo pip install -e /home/travis/build/urschrei/setup.py
+sudo pip install -e /home/travis/build/urschrei
