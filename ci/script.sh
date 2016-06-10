a0dcdfc4c483a8ebd196431b28c219537888827e
Stephan Hügel <urschrei@gmail.com>
2016-06-10 13:35:03 +0200

    Move tests to script

--- ci/script.sh
+++ ci/script.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -e -x
-if [[ "$DOCKER_BUILD" == "TRUE" ]]; then
+ls $HOME/build/urschrei/convertbng/convertbng
+fi
+
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+    cd $HOME/build/urschrei/convertbng && source venv/bin/activate
+fi
+# run the tests!
+cd $HOME/build/urschrei/convertbng && nosetests -v --with-xcoverage --cover-package=convertbng --cover-tests
+
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     docker run --rm -v `pwd`:/io $DOCKER_IMAGE $PRE_CMD /io/ci/build_wheel.sh
     ls wheelhouse/
-fi
