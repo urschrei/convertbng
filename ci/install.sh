73404d6d8e2698df9eca98458151600a5f303bf3
Stephan Hügel <urschrei@gmail.com>
2016-06-10 11:40:14 +0200

    Move more logic into pre-install script

--- ci/install.sh
+++ ci/install.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 set -e -x
-if [[ "$DOCKER_BUILD" == "TRUE" ]]; then
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     docker pull $DOCKER_IMAGE
     sudo pip install pip --upgrade
     sudo pip install --install-option="--no-cython-compile" cython
     sudo pip install numpy
     sudo pip install python-coveralls
     sudo pip install nosexcover
+    cd $HOME/build/urschrei/convertbng && sudo pip install -e .
 fi
-ls $HOME/build/urschrei/convertbng
-if [[ "$OSX_BUILD" == "TRUE" ]]; then
+
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     echo "OSX!"
     cc --version
     source venv/bin/activate
@@ -18,8 +19,9 @@ if [[ "$OSX_BUILD" == "TRUE" ]]; then
     pip install numpy
     pip install python-coveralls
     pip install nosexcover
+    cd $HOME/build/urschrei/convertbng && pip install -e .
     # otool -L $HOME/build/urschrei/convertbng/convertbng/liblonlat_bng.dylib
     # mkdir -p $HOME/build/urschrei/lonlat_bng/target/x86_64-apple-darwin/release/
     # cp $HOME/build/urschrei/convertbng/convertbng/liblonlat_bng.dylib $HOME/build/urschrei/lonlat_bng/target/x86_64-apple-darwin/release/ 
 fi
-cd $HOME/build/urschrei/convertbng && sudo pip install -e .
+
