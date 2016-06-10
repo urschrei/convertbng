6293942fcd1e06f190e2e23f1d675a0da75cbedc
Stephan Hügel <urschrei@gmail.com>
2016-06-10 14:47:16 +0200

    Remove nested convertbng folder

--- ci/install.sh
+++ ci/install.sh
@@ -16,12 +16,13 @@ fi
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     source $HOME/build/urschrei/convertbng/venv/bin/activate
     pip install pip --upgrade
-    # pip install --install-option="--no-cython-compile" cython
+    pip install --install-option="--no-cython-compile" cython
     pip install numpy
     pip install python-coveralls
     pip install nosexcover
     echo "OSX!"
     mv $HOME/build/urschrei/convertbng/convertbng/convertbng/liblonlat_bng.dylib $HOME/build/urschrei/convertbng/convertbng/
+    rm -rf $HOME/build/urschrei/convertbng/convertbng/convertbng/
     ls $HOME/build/urschrei/convertbng
     ls $HOME/build/urschrei/convertbng/convertbng
     pip install -e $HOME/build/urschrei/convertbng
