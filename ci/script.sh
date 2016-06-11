623fc0c393d2716f783c44c06958ef43cbc92d25
Stephan Hügel <urschrei@gmail.com>
2016-06-11 17:10:30 +0200

    simplify script

--- ci/script.sh
+++ ci/script.sh
@@ -11,14 +11,10 @@ nosetests -v --with-xcoverage --cover-package=convertbng --cover-tests
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     docker pull $DOCKER_IMAGE
     docker run --rm -v `pwd`:/io $DOCKER_IMAGE $PRE_CMD /io/ci/build_wheel.sh
-    pwd
-    ls wheelhouse/
 fi
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    mkdir wheelhouse
+    mkdir -p $HOME/build/urschrei/convertbng/wheelhouse
     python setup.py bdist_wheel
-    cp dist/*.whl wheelhouse
-    pwd
-    ls wheelhouse/
+    cp dist/*.whl $HOME/build/urschrei/convertbng/wheelhouse
 fi
