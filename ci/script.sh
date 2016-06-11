56823b0da611d4e8fac9a782dd14efb74e1421fc
Stephan Hügel <urschrei@gmail.com>
2016-06-11 16:58:00 +0200

    See where we are

--- ci/script.sh
+++ ci/script.sh
@@ -11,6 +11,7 @@ nosetests -v --with-xcoverage --cover-package=convertbng --cover-tests
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     docker pull $DOCKER_IMAGE
     docker run --rm -v `pwd`:/io $DOCKER_IMAGE $PRE_CMD /io/ci/build_wheel.sh
+    pwd
     ls wheelhouse/
 fi
 
@@ -18,5 +19,6 @@ if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     mkdir wheelhouse
     python setup.py bdist_wheel
     cp dist/*.whl wheelhouse
+    pwd
     ls wheelhouse/
 fi
