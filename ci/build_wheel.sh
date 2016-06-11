6a938a0b1f14d193ff03ad7c479fa6f056e7ef5e
Stephan Hügel <urschrei@gmail.com>
2016-06-11 19:24:06 +0200

    See what the io dir contents are

--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -7,7 +7,9 @@ yum install -y atlas-devel
 /opt/python/cp27-cp27m/bin/pip install -r /io/dev-requirements.txt
 # /opt/python/cp27-cp27m/bin/pip wheel /io/ -w wheelhouse/
 cd io && /opt/python/cp27-cp27m/bin/python setup.py bdist_wheel
-cp io/dist/*.whl io/wheelhouse
+ls io
+ls io/dist
+# cp io/dist/* io/wheelhouse
 
 # Bundle external shared libraries into the wheels
 for whl in io/wheelhouse/*.whl; do
