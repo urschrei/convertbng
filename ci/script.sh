f04f35c7ba4d2b8b21fd8a7cdc20c177cd1d3174
Stephan Hügel <urschrei@gmail.com>
2016-06-11 16:42:12 +0200

    Simplify script a bit

--- ci/script.sh
+++ ci/script.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 set -e -x
-ls $HOME/build/urschrei/convertbng/convertbng
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    cd $HOME/build/urschrei/convertbng && source venv/bin/activate
+    source venv/bin/activate
 fi
 
 # run the tests!
-cd $HOME/build/urschrei/convertbng && nosetests -v --with-xcoverage --cover-package=convertbng --cover-tests
+nosetests -v --with-xcoverage --cover-package=convertbng --cover-tests
 
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     docker pull $DOCKER_IMAGE
@@ -16,8 +15,7 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
 fi
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    pwd
     python setup.py bdist_wheel
-    cp $HOME/build/urschrei/convertbng/dist/*.whl wheelhouse
+    cp dist/*.whl wheelhouse
+    ls wheelhouse/
 fi
-ls wheelhouse
