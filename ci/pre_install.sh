1b291c7aa97daf6d50b806aced0ccad429dcde6d
Stephan Hügel <urschrei@gmail.com>
2016-06-24 13:59:37 +0200

    Run all pre-Docker Linux commands with sudo

--- ci/pre_install.sh
+++ ci/pre_install.sh
@@ -2,8 +2,8 @@
 set -e -x
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     sudo pip install pip --upgrade
-    sudo pip install requests[security]
-    python ci/pre_install.py
+    sudo pip install requests
+    sudo python ci/pre_install.py
 fi
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
