b53bf8948f136ad4a2f4e8dada46d892017067c3
Stephan Hügel <urschrei@gmail.com>
2016-06-24 15:28:23 +0200

    Typo fix

--- ci/install.sh
+++ ci/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e -x
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; the then
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     # install OSX
     source ci/travis_osx_steps.sh
     before_install
