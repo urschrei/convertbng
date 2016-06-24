86267fa0f2649b99a28dc78acd593d5d41ec8f8c
Stephan Hügel <urschrei@gmail.com>
2016-06-24 16:03:35 +0200

    make coveralls work again

--- ci/pre_install.sh
+++ ci/pre_install.sh
@@ -2,12 +2,8 @@
 set -e -x
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     sudo pip install pip --upgrade
+    sudo pip install nosexcover
+    sudo pip install python-coveralls
     sudo pip install requests
     sudo python ci/pre_install.py
-fi
-
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    # install OSX
-    source ci/travis_osx_steps.sh
-    before_install
 fi
