624e764da0cb87ab70707f8745d74eedc2a5147e
Stephan Hügel <urschrei@gmail.com>
2016-06-24 17:22:36 +0200

    Ensure we're installing everything we need for coverage testing

--- ci/pre_install.sh
+++ ci/pre_install.sh
@@ -2,10 +2,9 @@
 set -e -x
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     sudo pip install pip --upgrade
-    sudo pip install nose
+    sudo pip install -r dev-requirements.txt
     sudo pip install nosexcover
     sudo pip install python-coveralls
-    sudo pip install requests
     sudo python ci/pre_install.py
 fi
 
