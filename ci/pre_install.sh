631456993c0d48c5a5dac3668b93e7259b4aab0d
Stephan Hügel <urschrei@gmail.com>
2016-06-24 16:20:47 +0200

    Typo fix

--- ci/pre_install.sh
+++ ci/pre_install.sh
@@ -8,8 +8,8 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     sudo python ci/pre_install.py
 fi
 
--if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
- -    # install OSX
- -    source ci/travis_osx_steps.sh
- -    before_install
- -fi
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+    # install OSX
+    source ci/travis_osx_steps.sh
+    before_install
+ fi
