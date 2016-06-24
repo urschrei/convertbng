644a8044ff156c297d1d3e339b9e4daae5c39230
Stephan Hügel <urschrei@gmail.com>
2016-06-24 17:17:39 +0200

    Only run coverage tests on Linux

--- ci/pre_install.sh
+++ ci/pre_install.sh
@@ -2,6 +2,7 @@
 set -e -x
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     sudo pip install pip --upgrade
+    sudo pip install nose
     sudo pip install nosexcover
     sudo pip install python-coveralls
     sudo pip install requests
