92ed3b3aa5ab9ffbbe610442c82458ff7042afa1
Stephan Hügel <urschrei@gmail.com>
2016-06-24 16:17:49 +0200

    Re-add OSX preinstall directives

--- ci/pre_install.sh
+++ ci/pre_install.sh
@@ -7,3 +7,9 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     sudo pip install requests
     sudo python ci/pre_install.py
 fi
+
+-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+ -    # install OSX
+ -    source ci/travis_osx_steps.sh
+ -    before_install
+ -fi
