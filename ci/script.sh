7adc582f4ae84557e2af0083ed910704e9b0dcb2
Stephan Hügel <urschrei@gmail.com>
2016-06-24 14:55:44 +0200

    Run nosetests after install

--- ci/script.sh
+++ ci/script.sh
@@ -10,7 +10,7 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
 fi
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    python setup.py nosetests
+    # python setup.py nosetests
     pip wheel . -w wheelhouse
     ls wheelhouse
     mkdir to_test
