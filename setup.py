98f22a391152d53c32c5d44a4d43dfd44805b2a4
Stephan Hügel <urschrei@gmail.com>
2016-06-25 11:47:11 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -93,7 +93,6 @@ setup(
     classifiers=[
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
