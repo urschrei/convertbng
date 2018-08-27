44f36780d4bbcf0ffb384fbbee73e7a4267da487
Stephan Hügel <shugel@tcd.ie>
2018-08-27 17:32:44 +0100

    Show latest release

--- ci/pre_install.py
+++ ci/pre_install.py
@@ -28,6 +28,7 @@ project = 'lonlat_bng'
 latest_release = requests.get(
     "https://api.github.com/repos/urschrei/%s/releases/latest" % project,
     headers={'Authorization':'token %s' % ghkey}).json()
+print("Latest release: %s" % latest_release)
 # print(latest_release)
 tagname = latest_release['tag_name']
 # What platform are we on?
