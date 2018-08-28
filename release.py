902a9aae73542339a28d095f777f8d3e6f354636
Stephan Hügel <shugel@tcd.ie>
2018-08-28 13:04:25 +0100

    Update release mechanism

--- release.py
+++ release.py
@@ -12,45 +12,64 @@ import io
 import tarfile
 import zipfile
 import requests
-from multiprocessing import Pool
 from subprocess import check_output
+from multiprocessing import Pool
 
 path = 'dist/'
 url = "https://github.com/urschrei/convertbng/releases/download/{tag}/convertbng-{tag}-{target}.{extension}"
 # get latest tag
-tag = check_output(["git", "describe", "--abbrev=0"]).strip()
-
+tag = check_output(["git", "describe", "--abbrev=0", "--tags"]).strip()
 releases = [
-{
-    'tag': tag,
-    'target': 'x86_64-apple-darwin-cp27',
-    'extension': 'tar.gz'
-    },
-{
-    'tag': tag,
-    'target': 'x86_64-apple-darwin-cp36',
-    'extension': 'tar.gz'
-    },
-{
-    'tag': tag,
-    'target': 'x86_64-unknown-linux-gnu',
-    'extension': 'tar.gz'
-    },
-{
-    'tag': tag,
-    'target': 'x86_64-pc-windows-gnu-cp27',
-    'extension': 'zip'
-    },
-{
-    'tag': tag,
-    'target': 'i686-pc-windows-gnu-cp27',
-    'extension': 'zip'
-    },
-{
-    'tag': tag,
-    'target': 'x86_64-pc-windows-gnu-cp34',
-    'extension': 'zip'
-    }
+    {
+        'tag': tag,
+        'target': 'x86_64-apple-darwin-cp27',
+        'extension': 'tar.gz'
+        },
+    {
+        'tag': tag,
+        'target': 'x86_64-apple-darwin-cp35',
+        'extension': 'tar.gz'
+        },
+    {
+        'tag': tag,
+        'target': 'x86_64-apple-darwin-cp36',
+        'extension': 'tar.gz'
+        },
+    {
+        'tag': tag,
+        'target': 'x86_64-unknown-linux-gnu',
+        'extension': 'tar.gz'
+        },
+    {
+        'tag': tag,
+        'target': 'x86_64-pc-windows-msvc-cp27',
+        'extension': 'zip'
+        },
+    {
+        'tag': tag,
+        'target': 'i686-pc-windows-msvc-cp27',
+        'extension': 'zip'
+        },
+    {
+        'tag': tag,
+        'target': 'x86_64-pc-windows-msvc-cp35',
+        'extension': 'zip'
+        },
+    {
+        'tag': tag,
+        'target': 'i686-pc-windows-msvc-cp35',
+        'extension': 'zip'
+        },
+    {
+        'tag': tag,
+        'target': 'x86_64-pc-windows-msvc-cp36',
+        'extension': 'zip'
+        },
+    {
+        'tag': tag,
+        'target': 'i686-pc-windows-msvc-cp36',
+        'extension': 'zip'
+        },
 ]
 
 def retrieve(url):
@@ -70,4 +89,8 @@ urls = (url.format(**release) for release in releases)
 
 # let's do this in parallel
 pool = Pool(processes=5)
-pool.map(retrieve, urls)
+# we could use map, but it consumes the entire iterable (doesn't matter for small n)
+res = pool.map_async(retrieve, urls)
+# need these if we use _async
+pool.close()
+pool.join()
