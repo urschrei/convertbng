23938267d92990bf849db5b6e0cb1577e667a429
Stephan Hügel <shugel@tcd.ie>
2019-01-19 11:34:32 +0000

    Slightly better release util
    [ci skip]

--- release.py
+++ release.py
@@ -14,6 +14,7 @@ import zipfile
 import requests
 from subprocess import check_output
 from multiprocessing import Pool
+from urlparse import urlsplit
 
 path = 'dist/'
 url = "https://github.com/urschrei/convertbng/releases/download/{tag}/convertbng-{tag}-{target}.{extension}"
@@ -39,6 +40,16 @@ releases = [
         'tag': tag,
         'target': 'x86_64-apple-darwin-cp37',
         'extension': 'tar.gz'
+        },
+    {
+        'tag': tag,
+        'target': 'x86_64-pc-windows-msvc-cp37',
+        'extension': 'zip'
+        },
+    {
+        'tag': tag,
+        'target': 'i686-pc-windows-msvc-cp27',
+        'extension': 'zip'
         },
     {
         'tag': tag,
@@ -79,6 +90,7 @@ releases = [
 
 def retrieve(url):
     sess = requests.Session()
+    print("Getting %s" % urlsplit(url).path.split('/')[-1])
     retrieved = sess.get(url, stream=True)
     # don't continue if something's wrong
     retrieved.raise_for_status()
@@ -93,7 +105,7 @@ def retrieve(url):
 urls = (url.format(**release) for release in releases)
 
 # let's do this in parallel
-pool = Pool(processes=5)
+pool = Pool(processes=len(releases))
 # we could use map, but it consumes the entire iterable (doesn't matter for small n)
 res = pool.map_async(retrieve, urls)
 # need these if we use _async
