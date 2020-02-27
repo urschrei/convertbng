fa0878764c7ec16ce4b05c9f3235973aea276bf8
Stephan Hügel <shugel@tcd.ie>
2020-02-27 16:11:38 +0000

    Version bump

--- setup.py
+++ setup.py
@@ -94,8 +94,8 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7'
-        'Programming Language :: Python :: 3.8'
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
