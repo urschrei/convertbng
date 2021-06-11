453f6f81c32ffc6591a876c3cb444256d17d97c3
Stephan Hügel <shugel@tcd.ie>
2021-06-11 11:39:36 +0100

    Update metadata

--- setup.py
+++ setup.py
@@ -105,6 +105,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
